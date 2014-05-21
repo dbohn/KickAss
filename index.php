e581d968e8c94a86e841de6de6fab4c2db23bebb
David Bohn <david@cancrisoft.net>
2014-05-21 14:00:45 +0200

    Initial project layout

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+require 'vendor/autoload.php';
+
+$app = new \Slim\Slim(array(
+    'view' => new \Slim\Views\Twig(),
+    'templates.path' => 'views'
+));
+
+$app->view()->parserExtensions = array(
+    new \Slim\Views\TwigExtension(),
+);
+
+$app->get('/', function () use ($app) {
+    //echo "Hello, $name";
+    $app->render('index.twig');
+});
+$app->run();
