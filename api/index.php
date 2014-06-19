8ae9750879b7d8fc94ac411f79b4e65476393d6d
Luca <info@lucakeidel.de>
2014-06-19 11:53:47 +0200

    Tabelle added

--- api/index.php
+++ api/index.php
@@ -48,7 +48,7 @@ $app->group('/games',function () use ($app) {
       }
     });
 
-    $app->get('/spieltag/:spieltag/:hour/:minute', function($spieltag, $hour, $minute) use ($app){
+    $app->get('/spieltag/:spieltag(/:hour/:minute)', function($spieltag, $hour = 0, $minute = 0) use ($app){
 
       // catch trivial errors
       if($hour < 0 || $hour > 23 || $minute < 0 || $minute > 59){
@@ -83,6 +83,49 @@ $app->group('/games',function () use ($app) {
 
 // end group games
 
+$app->get('/tabelle(/:liga)', function ($liga = 1) use ($app){
+
+$query = <<<QUERY
+SELECT Verein.id, Verein.name, (gewonnen + unentschieden + verloren) as spieltage, q1.gewonnen, q2.unentschieden, q3.verloren, (q4.heimtore+q5.gasttore) AS tore, (gegenGast+gegenHeim) as gegentore, ((heimtore+gasttore) - (gegenGast+gegenHeim)) AS diff, (q1.gewonnen*3+q2.unentschieden) AS punkte
+ FROM verein, team, (SELECT Verein.id, Verein.name, COUNT(Spiel.id) AS gewonnen FROM
+    Verein
+    LEFT JOIN Spiel on (Verein.id = Spiel.gastgeber_id AND Spiel.toreHeim > Spiel.toreGast) OR (Verein.id = Spiel.gast_id AND Spiel.toreGast > Spiel.toreHeim)
+    GROUP BY Verein.id
+
+    ORDER BY Verein.id) as q1, (SELECT Verein.id, Verein.name, COUNT(Spiel.id) AS unentschieden FROM
+    Verein
+    LEFT JOIN Spiel on (Verein.id = Spiel.gastgeber_id AND Spiel.toreHeim = Spiel.toreGast) OR (Verein.id = Spiel.gast_id AND Spiel.toreGast = Spiel.toreHeim)
+    GROUP BY Verein.id
+
+    ORDER BY Verein.id) as q2, (SELECT Verein.id, Verein.name, COUNT(Spiel.id) AS verloren FROM
+    Verein
+    LEFT JOIN Spiel on (Verein.id = Spiel.gastgeber_id AND Spiel.toreHeim < Spiel.toreGast) OR (Verein.id = Spiel.gast_id AND Spiel.toreGast < Spiel.toreHeim)
+    GROUP BY Verein.id
+
+    ORDER BY Verein.id) as q3, (SELECT Verein.id, sum(toreHeim) AS heimtore FROM
+    Verein
+    LEFT JOIN Spiel on (Verein.id = gastgeber_id) GROUP BY Verein.id) as q4, (SELECT Verein.id, sum(toreGast) AS gasttore FROM
+    Verein
+    LEFT JOIN Spiel on (Verein.id = gast_id) GROUP BY Verein.id) as q5, (SELECT Verein.id, sum(toreHeim) AS gegenGast FROM
+    Verein
+    LEFT JOIN Spiel on (Verein.id = gast_id) GROUP BY Verein.id) as q6, (SELECT Verein.id, sum(toreGast) AS gegenHeim FROM
+    Verein
+    LEFT JOIN Spiel on (Verein.id = gastgeber_id) GROUP BY Verein.id) as q7 WHERE verein.id = q1.id and verein.id = q2.id and verein.id = q3.id and verein.id=q4.id and verein.id=q5.id and verein.id=q6.id and verein.id=q7.id and team.verein_id = verein.id and team.liga_id=:liga
+    ORDER BY punkte DESC
+QUERY;
+
+$q = new \API\PGQuery($query);
+try{
+  $resSet = $q -> exec(array('liga' => $liga));
+  $sql = $q -> getSQL();
+}catch(Exception $e){
+  error($e->getMessage());
+}
+
+resp($sql, $resSet);
+
+});
+
 
 
 $app->group('/list',function () use ($app) {
@@ -102,7 +145,7 @@ $app->group('/list',function () use ($app) {
         $end = new \DateTime($res['end_datum']);
 
         $id = $res['liga'].'.'.$res['start_datum'];
-        $name = 'Saison '.$start->format('y').'/'.$end->format('y').' '.$res['name'];
+        $name = $res['name'].' Saison '.$start->format('y').'/'.$end->format('y');
         $out[] = array('id' => $id, 'name' => $name);
 
       }
